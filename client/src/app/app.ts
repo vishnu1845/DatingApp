d79256cf144e8e69a693e251e8b3ef1c6b6627b2
Vishnu <vishnubandgar36@gmail.com>
2026-02-01 01:19:04 +0530

    end of session3

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +10,34 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App implements OnInit{
+
   private http = inject(HttpClient);
   protected readonly title = signal('Dating app');
+  protected members = signal<any>([]);
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
-      next: response => console.log(response),
-      error: error => console.log(error),
-      complete: ()=> console.log(`Completed the http request`)
-    })
+  // using observable
+  // ngOnInit(): void {
+  //   this.http.get('https://localhost:5001/api/members').subscribe({
+  //     // next: response => console.log(response),
+  //     next: response => this.members.set(response),
+  //     error: error => console.log(error),
+  //     complete: ()=> console.log(`Completed the http request`)
+  //   })
+  // }
+
+
+  // using promise
+  async ngOnInit(){
+    this.members.set(await this.getMembers())
+  }
+  async getMembers() {
+    try {
+      // return this.http.get('https://localhost:5001/api/members').toPromise()  //get deprecated
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 
 }
